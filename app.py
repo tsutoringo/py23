beca6f1d0b0d1961b36b791360c0de9c8e2f53ef
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2024-11-13 14:56:11 +0900

    naaaaaa

--- app.py
+++ app.py
@@ -1,38 +1,94 @@
-from flask import Response, request, Flask, make_response, render_template
-from datetime import datetime
-import json
+ #*******************************************************
+ # PY24 課題1 【MySQL接続】
+ # CLASS : IH-12A-405(16) NAME : goto akio
+ #-------------------------------------------------------
+ # 2024-10-16
+ #*******************************************************
+from flask import Flask,render_template, make_response, request
+import mysql.connector
+import os
 
-
+# 各種変数初期化
 app = Flask(__name__)
 
+# fild
+fild = {
+  "gno":"学籍番号",
+  "gname":"学生名",
+  "addr":"住所",
+  "tel":"電話番号",
+  "detail":"詳細"
+}
 
-@app.route("/", methods = [ "GET" ])
+def render_error(message: str, error: Exception):
+  print(f"********************* システム運用エラー *********************")
+  print(type(error))
+  print(error)
+
+  temp = render_template("error.html", message = message, code = type(error))
+  res = make_response(temp, 500)
+
+  return res
+
+# DB接続関数
+def con_db():
+  con = mysql.connector.connect(
+    host = "pythondatabase" if os.path.exists("/.dockerenv") else "localhost",
+    user = "root",
+    passwd = "root",
+    db = "py24db",
+  )
+  return con
+
+#****************************************************
+#（'/'）
+#****************************************************
+@app.route('/')
 def index():
-    return render_template("index.html")
 
-@app.route("/setcookie", methods = [ "POST" ])
-def set_cookie():
-    expires = int(datetime.now().timestamp()) + 3
-    response = make_response(render_template("setcookie.html"))
-    username = request.form["username"]
-    address = request.form["address"]
-    user_info = {'username': username, 'address': address}
+  recs = None
+  
+  try:
+    sql = "SELECT * FROM gakuseki;"
+    con = con_db()
+    cur = con.cursor(dictionary=True) 
+    cur.execute(sql)
+    recs = cur.fetchall()
+    cur.close()
+    con.close()
+  except mysql.connector.errors.DatabaseError as error:
+    return render_error("データーベーサーバーの起動が確認できません。", error)
+  except mysql.connector.errors.ProgrammingError as error:
+    return render_error("データベースプログラミングエラー", error)
+  except Exception as error:
+    return render_error("予期せぬエラーが発生しました。", error)
+  return render_template("index.html", recs=recs, fild=fild)
 
-    response.set_cookie("user", value=json.dumps(user_info), expires=expires)
+@app.route('/detail')
+def detail():
+  gno = request.args["gno"]
+  rec = None
+  
+  try:
+    sql = f"SELECT * FROM gakuseki where gno = {gno};"
+    con = con_db()
+    cur = con.cursor(dictionary=True) 
+    cur.execute(sql)
+    rec = cur.fetchall()[0]
+    cur.close()
+    con.close()
+  except mysql.connector.errors.DatabaseError as error:
+    return render_error("データーベーサーバーの起動が確認できません。", error)
+  except mysql.connector.errors.ProgrammingError as error:
+    return render_error("データベースプログラミングエラー", error)
+  except Exception as error:
+    return render_error("予期せぬエラーが発生しました。", error)
 
-    return response
+  return render_template("detail.html", rec=rec, fild=fild)
 
-@app.route("/getcookie", methods = [ "GET" ])
-def get_cookie():
-    user = request.cookies.get('user')
-    if user is None:
-        return render_template("error.html")
-    else:
-        user = json.loads(user)
-        return render_template("getcookie.html", user=user)
-
-
-
+#****************************************************
+# アプリケーション実⾏
+#****************************************************
 if __name__ == "__main__":
-    app.debug=True
-    app.run(host="0.0.0.0", port=5000)
+  app.debug=True #開発時デバックMODE
+  app.run(host="0.0.0.0", port=5000)
